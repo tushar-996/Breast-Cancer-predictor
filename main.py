8119733fb33eb9fdd43f41a7b6a961461c6df752
Tushar Chauhan <tchauhan@cae.com>
2020-05-01 00:51:12 -0700

    UI and data updates

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ file.close
 def hello_world():
     if request.method == "POST":
         myDict = request.form
+<<<<<<< HEAD
+        #errorMessage = ""
+=======
         errorMessage = ""
+>>>>>>> 447f8cb7052d1bf3a626d19db087db50a2f0f4c5
         #if myDict['fever'] == '' or myDict['age'] == '' or myDict['pain'] == '':
             #errorMessage = "Please enter all the values"
            # return render_template('index.html', error = errorMessage)
@@ -25,7 +29,11 @@ def hello_world():
         smoothness_mean = float(myDict['smoothness_mean'])
         compactness_mean = float(myDict['compactness_mean'])
         concavity_mean = float(myDict['concavity_mean'])
+<<<<<<< HEAD
+        concave_points_mean = float(myDict['concave_points_mean'])
+=======
         concave_points_mean = float(myDict['concave points_mean'])
+>>>>>>> 447f8cb7052d1bf3a626d19db087db50a2f0f4c5
         symmetry_mean = float(myDict['symmetry_mean'])
         fractal_dimension_mean = float(myDict['fractal_dimension_mean'])
         radius_se = float(myDict['radius_se'])
@@ -35,7 +43,11 @@ def hello_world():
         smoothness_se = float(myDict['smoothness_se'])
         compactness_se = float(myDict['compactness_se'])
         concavity_se = float(myDict['concavity_se'])
+<<<<<<< HEAD
+        concave_points_se = float(myDict['concave_points_se'])
+=======
         concave_points_se = float(myDict['concave points_se'])
+>>>>>>> 447f8cb7052d1bf3a626d19db087db50a2f0f4c5
         symmetry_se = float(myDict['symmetry_se'])
         fractal_dimension_se = float(myDict['fractal_dimension_se'])
         radius_worst = float(myDict['radius_worst'])
@@ -45,11 +57,19 @@ def hello_world():
         smoothness_worst = float(myDict['smoothness_worst'])
         compactness_worst = float(myDict['compactness_worst'])
         concavity_worst = float(myDict['concavity_worst'])
+<<<<<<< HEAD
+        concave_points_worst = float(myDict['concave_points_worst'])
+        symmetry_worst = float(myDict['symmetry_worst'])
+        fractal_dimension_worst = float(myDict['fractal_dimension_worst'])
+
+        inputFeatures = [radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst]
+=======
         concave_points_worst = float(myDict['concave points_worst'])
         symmetry_worst = float(myDict['symmetry_worst'])
         fractal_dimension_worst = float(myDict['fractal_dimension_worst'])
 
         inputFeatures = [radius_mean, texture_mean, 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave_points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave_points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst']
+>>>>>>> 447f8cb7052d1bf3a626d19db087db50a2f0f4c5
         infProb = clf.predict_proba([inputFeatures])[0][1]
         print(infProb)
         return render_template('show.html', inf = infProb)
--- myTraining.py
+++ myTraining.py
@@ -1,10 +1,39 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+<<<<<<< HEAD
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+=======
+>>>>>>> 447f8cb7052d1bf3a626d19db087db50a2f0f4c5
 import pickle
 
 
 
+<<<<<<< HEAD
+def data_split(data):
+
+    data = data.iloc[:, 0:32]
+    x =  data[['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave_points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave_points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst']].to_numpy()
+    y =  data[['diagnosis']].to_numpy()
+    labeled = LabelEncoder()
+    y = labeled.fit_transform(y)
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.25, random_state = 0)
+    ss = StandardScaler()
+    x_train = ss.fit_transform(x_train)
+    x_test = ss.transform(x_test)
+
+    return x_train, x_test, y_train, y_test
+
+if __name__ == "__main__":
+    df = pd.read_csv('data.csv')
+    X_train, X_test, Y_train, Y_test = data_split(df)
+
+    clf = LogisticRegression(random_state=0)
+=======
 def data_split(data, ratio):
     np.random.seed(42)
     shuffled = np.random.permutation(len(data))
@@ -23,6 +52,7 @@ if __name__ == "__main__":
     Y_test = test[['infectionProb']].to_numpy().reshape(499, )
 
     clf = LogisticRegression()
+>>>>>>> 447f8cb7052d1bf3a626d19db087db50a2f0f4c5
     clf.fit(X_train, Y_train)
 
     # open a file, where you ant to store the data
